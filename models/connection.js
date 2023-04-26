6bd46c7b26fcbe052af6a73be94e2feb9290c2ad
Praneet30 <131679934+Praneet30@users.noreply.github.com>
2023-04-26 16:48:57 +0530

    Add files via upload

---
+++ models/connection.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const connectionSchema=new mongoose.Schema({
+    personname:String,
+    personmail:String,
+    personphn:Number,
+    personAddress:String,
+    personArea:String,
+    status: {
+        type: String,
+        default: 'pending'
+      },
+    connectionId:{
+        type:String
+    }
+});
+
+const connection=new mongoose.model("connection",connectionSchema)
+
+module.exports = connection;
